100a607a734b5866cd01940e4d5de35f22156c7b
Jude Harding <jharding@genesisgroupusa.com>
2016-01-27 18:09:01 -0500

    code from last nights class

--- rpsnew.js
+++ rpsnew.js
@@ -1,8 +1,79 @@
 
-var hands = ["rock", "paper", "scissors"];
-
-function getHand(hands) {
-    hand = (parseInt(Math.random()*10)%3);
+// var hands = ['rock', 'paper', 'scissors'];
+​
+// function getHand(){
+// 	return hands[parseInt(Math.random()*10)%3];
+// }
+​
+// var player1 = {
+// 	name: 'Jude',
+// 	hand: getHand
+// }
+​
+// var player2 = {
+// 	name: 'Lee',
+// 	hand: getHand
+// }
+​
+// function playRound(player1, player2){
+// 	player1.hand();
+// 	player2.hand();
+​
+​
+// // We need to separate these into 2 functions
+// 	if (p1Choice === p2Choice){
+// 		console.log("You tied play again!");
+// 		return nil;
+// 	} else if p1Choice === 0 && p2Choice === 1){
+// 		console.log("Paper covers rock! " + player2.name + " wins");
+// 		return player2;
+// 	} else if p1Choice === 0 && p2Choice === 2){
+// 		console.log("Rock smashes scissors! " + player1.name + " wins");
+// 		return player1;
+// 	} else if p1Choice === 1 && p2Choice === 0){
+// 		console.log("Paper covers rock! " + player1.name + " wins");
+// 		return player1;
+// 	} else if p1Choice === 1 && p2Choice === 2){
+// 		console.log("Scissors cuts paper! " + player2.name + " wins");
+// 		return player2;
+// 	} else if p1Choice === 2 && p2Choice === 0){
+// 		console.log("Rock smashes scissors! " + player2.name + " wins");
+// 		return player2;
+// 	} else if p1Choice === 2 && p2Choice === 1){
+// 		console.log("Scissors cuts paper! " + player1.name + " wins");
+// 		return player1;
+// 	}
+// }
+​
+​
+​
+// console.log(playRound(player1, player2));
+​
+​
+function outer(){
+	var a = "Lee";
+	var b = {
+		key: "value"
+	};
+	console.log(a)
+	console.log(b)
+	function inner(a,b){
+		var a = "Jude"
+		var v = {}
+		b.name = "Assaf"
+		console.log(a)
+		console.log(b)
+	}
+​
+	inner(a,b);
+	console.log(a,b)
 }
-var player1 = {name:"Lee", getHand()};
-var player2 = {name:"Jude", getHand()};
+​
+outer();
+​
+function(obj) {
+	//do stuff to object
+	obj.newVal = "abc";
+}
+​
+​
