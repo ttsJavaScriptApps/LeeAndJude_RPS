82e99f5a37bdd3061e6212991610260fe1608165
Jude Harding <jharding@genesisgroupusa.com>
2016-01-27 14:13:46 -0500

    Getting Started

---
+++ rpsnew.js
@@ -0,0 +1,8 @@
+
+var hands = ["rock", "paper", "scissors"];
+
+function getHand(hands) {
+    hand = (parseInt(Math.random()*10)%3);
+}
+var player1 = {name:"Lee", getHand()};
+var player2 = {name:"Jude", getHand()};
