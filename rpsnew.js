a43f6d8320830b689b7681d2035300363b5db5a8
Jude Harding <jharding@genesisgroupusa.com>
2016-01-28 15:06:29 -0500

    pulled out exercise 3

--- rpsnew.js
+++ rpsnew.js
@@ -70,36 +70,5 @@ var playRound = function(player1, player2){
 //  		return player1;
 //  	}
 // }
-// ​
-​
-​
 // console.log(playRound(player1, player2));
 ​
-// ​
-// function outer(){
-// 	var a = "Lee";
-// 	var b = {
-// 		key: "value"
-// 	};
-// 	console.log(a)
-// 	console.log(b)
-// 	function inner(a,b){
-// 		var a = "Jude"
-// 		var v = {}
-// 		b.name = "Assaf"
-// 		console.log(a)
-// 		console.log(b)
-// 	}
-// ​
-// 	inner(a,b);
-// 	console.log(a,b)
-// }
-// ​
-// outer();
-// ​
-// function(obj) {
-// 	//do stuff to object
-// 	obj.newVal = "abc";
-// }
-// ​
-// ​
